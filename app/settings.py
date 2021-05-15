5f231a5689c24ff17b5e05258b131d6237eff64c
saeedcops <saeed_alaky@hotmail.com>
2021-05-15 11:09:09 +0200

    first commit

--- app/settings.py
+++ app/settings.py
@@ -198,11 +198,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "app/static")]
 
 # STATIC_URL = '/static/static/'
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_DIRS = [os.path.join(BASE_DIR, "app/media")]
+
 
 # STATIC_ROOT = os.path.join(BASE_DIR, "vol/web/static")
 # MEDIA_ROOT = os.path.join(BASE_DIR, "vol/web/media")
